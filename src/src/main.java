32058fee986fe843d3da7583424ae4957803a291
Amr Hany <anodiousname@gmail.com>
2017-12-11 15:40:22 +0200

    Working dummy
    Added some UI elements

--- src/src/main.java
+++ src/src/main.java
@@ -1,41 +1,153 @@
 package src;
+
 import org.jnetpcap.Pcap;
 import org.jnetpcap.PcapIf;
 import org.jnetpcap.packet.PcapPacket;
 import org.jnetpcap.packet.PcapPacketHandler;
 import org.jnetpcap.protocol.network.Ip4;
 
-import javax.swing.*;
-import java.awt.*;
+//import javax.swing.*;
+//import java.awt.*;
+//import java.util.ArrayList;
+//import java.util.List;
+
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import javafx.scene.layout.StackPane;
+
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class main {
 
-    public JFrame frame;
-    public JTextArea output;
+public class main extends Application {
 
-    public void render(String s []){
-        this.frame = new JFrame();
-        frame.getContentPane().setLayout(new FlowLayout());
-        frame.setSize(800, 800);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        for (int i = 0; i < s.length; i++) {
-            JLabel l = new JLabel(s[i]);
-            l.setBounds(50,25 * (i + 1),100,30);
-            frame.add(l);
-        }
+    @Override
+    public void start(Stage stage) throws Exception {
+//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+//        primaryStage.setTitle("Hello World");
+//        primaryStage.setScene(new Scene(root, 300, 275));
+//        primaryStage.show();
+
+        setupUI(stage);
+
+
     }
 
-    public static String[] capture(){
+    public void setupUI(Stage stage) {
+
+        stage.setTitle("Menu Sample");
+        Scene scene = new Scene(new VBox(), 400, 350);
+        scene.setFill(Color.OLDLACE);
+
+
+        MenuBar menuBar = new MenuBar();
+
+        TextArea textArea = new TextArea();
+        textArea.setPrefSize(400, 400);
+        textArea.setEditable(false);
+
+
+
+        // --- Menu File
+        Menu menuFile = new Menu("File");
+        MenuItem startCapture = new MenuItem("StartCapture");
+        startCapture.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent t) {
+                textArea.setText(capture());
+
+            }
+        });
+        MenuItem add2 = new MenuItem("Test");
+        add2.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent t) {
+
+
+
+            }
+        });
+
+        menuFile.getItems().addAll(startCapture, add2);
+
+        // --- Menu Edit
+        Menu menuEdit = new Menu("Edit");
+
+        // --- Menu View
+        Menu menuView = new Menu("View");
+
+        menuBar.getMenus().addAll(menuFile, menuEdit, menuView);
+
+
+        ((VBox) scene.getRoot()).getChildren().addAll(menuBar);
+
+
+        ObservableList<String> options =
+                FXCollections.observableArrayList(
+                        "Ethernet",
+                        "Wifi"
+                );
+        final ComboBox comboBox = new ComboBox(options);
+        ((VBox) scene.getRoot()).getChildren().addAll(comboBox);
+        ((VBox) scene.getRoot()).getChildren().addAll(textArea);
+
+
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+//
+//    public JFrame frame;
+//    public JTextArea output;
+//
+//    public void render(String s []){
+//        this.frame = new JFrame();
+//        frame.getContentPane().setLayout(new FlowLayout());
+//        frame.setSize(800, 800);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setVisible(true);
+//        for (int i = 0; i < s.length; i++) {
+//            JLabel l = new JLabel(s[i]);
+//            l.setBounds(50,25 * (i + 1),100,30);
+//            frame.add(l);
+//        }
+//    }
+
+    public static String capture() {
+
+        // Create a stream to hold the output
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos);
+        // IMPORTANT: Save the old System.out!
+        PrintStream old = System.out;
+        // Tell Java to use your special stream
+        System.setOut(ps);
+
+
         List<PcapIf> alldevs = new ArrayList<PcapIf>();
         StringBuilder errbuf = new StringBuilder(); // For any error msgs
-        String [] s = new String[10];
+        String[] s = new String[10];
         int r = Pcap.findAllDevs(alldevs, errbuf);
         if (r == Pcap.NOT_OK || alldevs.isEmpty()) {
             System.err.printf("Can't read list of devices, error is " + errbuf.toString());
-            return s;
+            return baos.toString();
         }
         System.out.println("Network devices found:");
         int i = 0;
@@ -56,7 +168,7 @@ public class main {
         if (pcap == null) {
             System.err.printf("Error while opening device for capture: "
                     + errbuf.toString());
-            return s;
+            return baos.toString();
         }
 
         final int[] j = {0};
@@ -76,12 +188,55 @@ public class main {
         };
         pcap.loop(10, jpacketHandler, "jNetPcap");
         pcap.close();
-        return s;
+
+        // Put things back
+        System.out.flush();
+        System.setOut(old);
+        // Show what happened
+        System.out.println("Here: " + baos.toString());
+        return baos.toString();
     }
 
-    public static void main(String[] args){
-        String [] s = main.capture();
-        main m = new main();
-        m.render(s);
+    public static void main(String[] args) {
+        launch(args);
+
+        //        try {
+//            byte[] buffer = new byte[1000];
+//
+//            FileInputStream inputStream = new FileInputStream("files/trace.pcap");
+//
+//            // read fills buffer with data and returns
+//            // the number of bytes read (which of course
+//            // may be less than the buffer size, but
+//            // it will never be more).
+//            int total = 0;
+//            int nRead = 0;
+//            while ((nRead = inputStream.read(buffer)) != -1) {
+//                // Convert to String so we can display it.
+//                // Of course you wouldn't want to do this with
+//                // a 'real' binary file.
+//                System.out.println(new String(buffer));
+//                total += nRead;
+//            }
+//
+//            // Always close files.
+//            inputStream.close();
+//
+//            System.out.println("Read " + total + " bytes");
+//        } catch (FileNotFoundException ex) {
+//            System.out.println(
+//                    "Unable to open file '");
+//        } catch (IOException ex) {
+//            System.out.println(
+//                    "Error reading file '");
+//            // Or we could just do this:
+//            // ex.printStackTrace();
+//        }
+//
+//
+//        String [] s = main.capture();
+//        main m = new main();
+//        m.render(s);
+//    }
     }
 }

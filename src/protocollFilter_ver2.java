812f746d5e0f691dfa66098f3c5c12aa2957ebc2
mahmuuud <34382489+mahmuuud@users.noreply.github.com>
2017-12-15 15:54:40 +0200

    test code for protocol filter "2 versions"

---
+++ src/protocollFilter_ver2.java
@@ -0,0 +1,56 @@
+
+import org.jnetpcap.Pcap;
+
+import org.jnetpcap.PcapBpfProgram;
+
+public class protocollFilter_ver2 {
+
+    public static void main(String[] args) {
+
+        StringBuilder errbuf = new StringBuilder();
+
+        String fname = "tests/test-afs.pcap";
+        Pcap pcap = Pcap.openOffline(fname, errbuf);
+
+        if (pcap == null) {
+
+            System.err.println(errbuf.toString());
+
+            return;
+
+        }
+
+        PcapBpfProgram program = new PcapBpfProgram();
+
+        String expression = "host 192.168.1.1";
+
+        int optimize = 0;         // 0 = false
+
+        int netmask = 0xFFFFFF00; // 255.255.255.0
+
+        if (pcap.compile(program, expression, optimize, netmask) != Pcap.OK) {
+
+            System.err.println(pcap.getErr());
+
+            return;
+
+        }
+
+        if (pcap.setFilter(program) != Pcap.OK) {
+
+            System.err.println(pcap.getErr());
+
+            return;
+
+        }
+        System.out.println("Filter set !!! : " + expression);
+
+        pcap.close();
+
+
+
+    }
+
+
+
+}
